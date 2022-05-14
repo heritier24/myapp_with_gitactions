747afebccc52a8cdc682f8437278a18f97602738
heritier24 <heritier.ganza@kudibooks.com>
2022-05-15 00:47:58 +0200

    added appy jobs migrations

---
+++ database/migrations/2022_05_14_214302_create_applyjobs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applyjobs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('candidateid');  // foreign key candidate id 
+            $table->integer('jobid');  // foreign key 
+            $table->date('date_applied');
+            $table->string('status');  // pending , Selected , Cancelled
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applyjobs');
+    }
+};

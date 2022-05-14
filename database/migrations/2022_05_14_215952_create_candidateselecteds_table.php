8e0d85dd28542f26f0dc59b85a320f9f508faab3
heritier24 <heritier.ganza@kudibooks.com>
2022-05-15 00:44:00 +0200

    added candidate selected model and migrations

---
+++ database/migrations/2022_05_14_215952_create_candidateselecteds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidateselecteds', function (Blueprint $table) {
+            $table->id();
+            $table->integer('applied_id');  // foreign key applyjobs
+            $table->date('date_selected');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidateselecteds');
+    }
+};

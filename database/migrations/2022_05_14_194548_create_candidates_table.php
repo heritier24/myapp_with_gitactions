0c8dfccf234bc245aa7eae85b2d3baac3174db7c
heritier24 <heritier.ganza@kudibooks.com>
2022-05-15 00:47:20 +0200

    added candidates migrations

---
+++ database/migrations/2022_05_14_194548_create_candidates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->integer('candidate_userid');  // candidate user id 
+            $table->string('candidate_names');
+            $table->string('candidate_phonenumber');
+            $table->string('candidate_email');
+            $table->string('nationalid');
+            $table->string('cv');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+};

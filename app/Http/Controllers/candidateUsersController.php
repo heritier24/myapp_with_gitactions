a4a0c7041ade35cd52e22567d82edacceaf52896
heritier24 <heritier.ganza@kudibooks.com>
2022-05-16 21:38:25 +0200

    candidate users registration, login and logout

---
+++ app/Http/Controllers/candidateUsersController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\candidates;
+use App\Models\candidateusers;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class candidateUsersController extends Controller
+{
+
+    // register users 
+    public function registerCandidate(Request $request){
+        $validation = Validator::make($request->all(), [
+            "name" => "required",
+            "email" => "required|email:rfc,dns",
+            "password" => "required|string|min:6|confirmed"
+        ]);
+
+        // if ($validation->fails()) {
+        //     return response()->json(["errors" => $validation->errors()->all()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        // }
+
+        $user = candidateusers::create([
+            'name' => $request->name,
+            'password' => Hash::make($request->password),
+            'email' => $request->email
+        ]);
+        $token = $user->createToken('token')->plainTextToken;
+
+        return \response()->json([
+            "token" => $token,
+        ]);
+    }
+
+    public function login(Request $request){
+        $validation = Validator::make($request->all(), [
+            "email" => "required|email:rfc,dns",
+            "password" => "required|string|min:6"
+        ]);
+
+        // if ($validation->fails()) {
+        //     return response()->json(["errors" => $validation->errors()->all()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        // }
+
+        $user = candidateusers::where("email", $request->email)->first();
+
+        if (!$user) {
+            return \response()->json(["errors" => ["User not found"]], Response::HTTP_NOT_FOUND);
+        }
+        if (!Hash::check($request->password, $user->password)) {
+            return \response()->json(["errors" => ["Invalid credentials"]], Response::HTTP_FORBIDDEN);
+        }
+
+        Auth::attempt([
+            "email" => $request->email,
+            "password" => $request->password
+        ]);
+
+        Auth::user()->tokens()->delete();
+
+        $id = Auth::id();
+        $token = Auth::user()->createToken('token')->plainTextToken;
+
+        return \response()->json([
+            "token" => $token,
+        ]);
+    }
+    public function logout(){
+        auth()->user()->tokens()->delete();
+
+
+        return response()->json([
+            'message' => 'Tokens Revoked'
+        ], Response::HTTP_OK);
+    }
+}

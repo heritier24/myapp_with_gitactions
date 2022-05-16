e5b9c7b2244de21f3ee35aa76d0a426e6b8992c8
heritier24 <heritier.ganza@kudibooks.com>
2022-05-16 21:37:29 +0200

    added authentication controller and installed laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\candidateUsersController;
+use App\Http\Controllers\PublishJobsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('/sign-up', [AuthenticationController::class, 'createAccount']);
+    Route::post('/signin', [AuthenticationController::class, 'signin']);
+    Route::get('/sign-out', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/listUsers', [AuthenticationController::class, 'listUsers']);
+});
+
+Route::get('/jobs', [PublishJobsController::class, 'index']);
+Route::post('/publish-jobs', [PublishJobsController::class, 'store']);
+Route::post('/apply-jobs', [PublishJobsController::class, 'applyJobs']);
+Route::get('/get-job/{id}', [PublishJobsController::class, 'getJobById']);
+
+Route::get('/candidate', [candidateUsersController::class, 'index']);
+Route::post('/register-candidate', [candidateUsersController::class, 'registerCandidate']);
+
